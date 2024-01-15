994988617784079e9495868a6ace010750e45065
Asvin Goel <goel@telematique.eu>
2024-01-15 09:46:03 +0700

    Initial commit

--- SubProcessImporter.js
+++ SubProcessImporter.js
@@ -50,40 +50,29 @@ export default class SubProcessImporter {
 
   loadFile(event,element) {
     // Check if exactly one file is selected
-    if (event.target.files.length == 1) {
-      // Get the selected file name
-      const fileName = event.target.files[0].name;
-
-      // Load the file
-      var xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = () => {
-        if (xhttp.readyState == 4 && xhttp.status == 200) {
-          // Create new modeler for import of the model
-          const sourceModeler = new BpmnModeler( { moddleExtensions: this.moddleExtensions } );
-          sourceModeler.importXML( xhttp.responseText ).then( () => {
-            const elementRegistry = sourceModeler.get('elementRegistry');
-            const processes = elementRegistry.filter(function (element) {
-              return is(element, 'bpmn:Process');
-            });
-            if ( processes.length == 1 ) {
-              // Copy content of process into the target subprocess
-              copyAndPaste(sourceModeler, processes[0], this.bpmnjs, element);
-            }
-            else {
-              console.warn('Failed to determine unique process.');
-            }
-          });
+    var reader = new FileReader();
+    reader.onload = (e) => {
+      // Create new modeler for import of the model
+      const sourceModeler = new BpmnModeler( { moddleExtensions: this.moddleExtensions } );
+      sourceModeler.importXML( reader.result ).then( () => {
+        const elementRegistry = sourceModeler.get('elementRegistry');
+        const processes = elementRegistry.filter(function (element) {
+          return is(element, 'bpmn:Process');
+        });
+        if ( processes.length == 1 ) {
+          // Copy content of process into the target subprocess
+          copyAndPaste(sourceModeler, processes[0], this.bpmnjs, element);
         }
         else {
-          console.warn('Failed to get file. ReadyState: ' + xhttp.readyState + ', Status: ' + xhttp.status);
+          console.warn('Failed to determine unique process.');
         }
-      };
-      xhttp.open('GET',fileName,true);
-      xhttp.send();
-    }
-    else {
-      console.warn("To import into a subprocess, exactly one file must be selected.");
-    }
+      });
+    };
+    reader.onerror = (err) => {
+      console.log(err,err.loaded,err.loaded === 0,file);
+    };
+
+    reader.readAsText(event.target.files[0]);
   }
 }
 
@@ -96,7 +85,7 @@ SubProcessImporter.inject = [
 // helper /////
 
 function hasChildren(element) {
-  return element.businessObject 
-    && element.businessObject.flowElements 
+  return element.businessObject
+    && element.businessObject.flowElements
     && element.businessObject.flowElements.length;
 }
