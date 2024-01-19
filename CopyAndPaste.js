c8ec05e8852d167e6af08bd995407f436688ea4e
Asvin Goel <goel@telematique.eu>
2024-01-19 11:30:40 +0300

    Allow customisation of copyAndPaste

--- CopyAndPaste.js
+++ CopyAndPaste.js
@@ -3,7 +3,15 @@ import { is } from 'bpmn-js/lib/util/ModelUtil';
 /**
  * Copies all children from a source element to a target element.
  */
-export default function copyAndPaste(sourceModeler, sourceElement, targetModeler, targetElement ) {
+export default function copyAndPaste(sourceModeler, sourceElement, targetModeler, targetElement, options ) {
+  const {
+    position,
+    ids
+  } = options || {
+    position: {x: 500, y: 200},
+    ids: null
+  };
+
   const sourceClipboard = sourceModeler.get('clipboard'),
         sourceCopyPaste = sourceModeler.get('copyPaste'),
         sourceElementRegistry = sourceModeler.get('elementRegistry');
@@ -12,12 +20,12 @@ export default function copyAndPaste(sourceModeler, sourceElement, targetModeler
   var elements = [];
   if ( is(sourceElement, 'bpmn:Process') ) {
     elements = sourceElementRegistry.getAll().filter(function(element) {
-      return element.parent && element.parent.id == sourceElement.id;
+      return element.parent && element.parent.id == sourceElement.id && ( !ids || ids.includes(element.id));
     });
   }
   else {
     elements = sourceElementRegistry.getAll().filter(function(element) {
-      return element.parent && element.parent.id == sourceElement.id + '_plane';
+      return element.parent && element.parent.id == sourceElement.id + '_plane' && ( !ids || ids.includes(element.id));
     });
   }
 
@@ -32,7 +40,7 @@ export default function copyAndPaste(sourceModeler, sourceElement, targetModeler
   targetClipboard.set(sourceClipboard.get());
   const planeElement = targetElementRegistry.get(`${targetElement.id}_plane`);
   // paste into target
-  targetCopyPaste.paste( { element: planeElement, point: {x: 500, y: 200} });
+  targetCopyPaste.paste( { element: planeElement, point: position });
 
   // clear clipboard
   targetClipboard.clear();
